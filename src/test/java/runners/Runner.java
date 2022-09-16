243963e2527676eefdcd12f0f2bcb9f3f28624ad
Sinan SARIKAYA <sarikayasinan25@gmail.com>
2022-09-16 17:57:03 +0300

    v23

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,20 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-features = "src/test/resources/features",
+        plugin={"html:target\\cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features="src/test/resources/features",
         glue="stepdefination",
-        tags = "@amazon",
+        tags="@Test1"  ,
 
-        dryRun=false
-
-
-)
-
+        dryRun= false)
 
 
 public class Runner {
-
-
-
-
 }
